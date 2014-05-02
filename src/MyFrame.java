dedf8e905a3f35821d91e9058fcca7ba3c77df5e
viver99 <jozef.curyllo+github@gmail.com>
2014-05-02 16:51:29 +0200

    Menu
    Menu with few example elements. Only GUI.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,11 +1,15 @@
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
 
 
 public class MyFrame {
 
 	JFrame frame;
-
-
 
 	/**
 	 * Create the application.
@@ -21,6 +25,39 @@ public class MyFrame {
 		frame = new JFrame("Business Travel Helper");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnFile.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnFile.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnFile.add(mntmSave);
+		
+		JMenuItem mntmPrint = new JMenuItem("Print");
+		mnFile.add(mntmPrint);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmHelp = new JMenuItem("Help");
+		mnHelp.add(mntmHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		
+		JLabel lblItIsOny = new JLabel("First text :)");
+		frame.getContentPane().add(lblItIsOny, BorderLayout.WEST);
 	}
 
 }
